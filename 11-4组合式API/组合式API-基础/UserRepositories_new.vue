1bfcd0d450a5da407dcce83cf9bf300ced1decd8
Mity1299 <807798223@qq.com>
2021-10-21 17:31:20 +0800

    测试组合式API,生命钩子、watch、和ref

--- 11-4组合式API/组合式API-基础/UserRepositories_new.vue
+++ 11-4组合式API/组合式API-基础/UserRepositories_new.vue
@@ -7,7 +7,7 @@ import RepositoriesFilters from './RepositoriesFilters.vue';
 import RepositoriesSortBy from './RepositoriesSortBy.vue';
 import RepositoriesList from './RepositoriesList.vue';
 import { fetchUserRepositories } from '@/api/repositories';
-import {ref} from 'vue';
+import { ref, onMounted, watch, toRefs} from 'vue'
 
 export default {
   components: { RepositoriesFilters, RepositoriesSortBy, RepositoriesList },
@@ -19,14 +19,26 @@ export default {
   },
   //setup 选项是一个接收 props 和 context 的函数
   //我们将 setup 返回的所有内容都暴露给组件的其余部分 (计算属性、方法、生命周期钩子等等) 以及组件的模板
+
   setup(props){
     console.log(props);
-    //这里返回的任何内容都可以用于组件的其余部分
+
+    // 使用 `toRefs` 创建对prop的 `user` property 的响应式引用
+    const { user } = toRefs(props)
 
     let repositories = ref([]);//响应式处理
     const getUserRepositories = async () => {
       repositories = await fetchUserRepositories(props.user);
     }
+
+    //生命周期钩子
+    onMounted(getUserRepositories);
+
+    //监听user，如果有变动则重新获取repositories
+    watch(user,getUserRepositories);
+
+
+    //这里返回的任何内容都可以用于组件的其余部分
     return {
       repositories,
       getUserRepositories,
